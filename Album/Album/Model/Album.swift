3af7fa97ec66276cd379ce7587abbdcba69e6a7e
Hayden Hastings <haydenhastings32@gmail.com>
2019-10-01 14:50:34 -0600

    Finished the Encoding method in the Album model. Also Finished the Model for Song.

--- Album/Album/Model/Album.swift
+++ Album/Album/Model/Album.swift
@@ -80,6 +80,35 @@ class Album: Codable {
     
     func encode(to encoder: Encoder) throws {
         
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(artist, forKey: .artist)
+        try container.encode(identifier, forKey: .id)
+        
+        var coverArtContainer = container.nestedUnkeyedContainer(forKey: .coverArt)
+        var urlArtContainer = coverArtContainer.nestedContainer(keyedBy: CodingKeys.CoverArtKeys.self)
+        
+        for url in coverArt {
+            try urlArtContainer.encode(url, forKey: .url)
+        }
+        
+        try container.encode(genres, forKey: .genres)
+        
+        var songsContainer = container.nestedUnkeyedContainer(forKey: .songs)
+        
+        for song in songs {
+            try songsContainer.encode(song)
+        }
+    }
+    
+    init(name: String, artist: String, identifier: UUID = UUID(), coverArt: [URL], genres: [String], songs: [Song]) {
+        self.name = name
+        self.artist = artist
+        self.identifier = identifier
+        self.coverArt = coverArt
+        self.genres = genres
+        self.songs = songs
     }
 }
 
@@ -87,4 +116,60 @@ class Album: Codable {
 
 class Song: Codable {
     
+    let title: String
+    let identifier: UUID
+    let duration: String
+    
+    enum CodingKeys: String, CodingKey {
+        case duration
+        case name
+        case id
+        
+        enum DurationCodingKeys: String, CodingKey {
+            case duration
+            case seconds
+        }
+        
+        enum NameCodingKeys: String, CodingKey {
+            case title
+        }
+    }
+    
+    init(title: String, identifier: UUID = UUID(), duration: String) {
+        self.title = title
+        self.identifier = identifier
+        self.duration = duration
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let identifier = try container.decode(UUID.self, forKey: .id)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        
+        let durationContainer = try container.nestedContainer(keyedBy: CodingKeys.DurationCodingKeys.self, forKey: .duration)
+        
+        let duration = try durationContainer.decode(String.self, forKey: .duration)
+        
+        self.title = title
+        self.identifier = identifier
+        self.duration = duration
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(identifier, forKey: .id)
+        
+        var nameContainer = container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        
+        try nameContainer.encode(title, forKey: .title)
+        
+        var durationContainer = container.nestedContainer(keyedBy: CodingKeys.DurationCodingKeys.self, forKey: .duration)
+        
+        try durationContainer.encode(duration, forKey: .duration)
+    }
 }
